cd525b68027347a0a054b916040f678fc0438621
Your Name <you@example.com>
2025-04-20 00:42:11 +0000

    reflector nav-link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,10 +13,13 @@
     <x-nav.nav />
 
     <nav class="bg-gray-100 px-6 py-2">
-        
-        <a href="/" class="{{ request()->is('/') ? 'bg-red-200' : '' }} mr-4 text-blue-700 hover:underline">Home</a>
-        <a href="/about" class="{{ request()->is('about') ? 'bg-red-200' : '' }} mr-4 text-blue-700 hover:underline">About</a>
-        {{-- <a href="/contact" class="text-blue-700 hover:underline">Contact</a> --}}
+        {{-- <a href="/" class="{{ request()->is('/') ? 'bg-red-200' : '' }} mr-4 text-blue-700 hover:underline">Home</a> --}}
+        {{-- <a href="/about" class="{{ request()->is('about') ? 'bg-red-200' : '' }} mr-4 text-blue-700 hover:underline">About</a> --}}
+        {{-- <x-nav.nav-link href="/" :active="true">Home</x-nav.nav-link> --}}
+        {{-- <x-nav.nav-link href="/" :active="false">Home</x-nav.nav-link> --}}
+
+        <x-nav.nav-link href="/" active="{{ request()->is('/') }}">Home</x-nav.nav-link>
+        <x-nav.nav-link href="/about" active="{{ request()->is('about') }}">About</x-nav.nav-link>
     </nav>
 
     <header>{{ $heading }}</header>
--- resources/views/components/nav/nav-link.blade.php
+++ resources/views/components/nav/nav-link.blade.php
@@ -1,1 +1,7 @@
-{{--  --}}
+@props(['active' => false, 'href' => '/'])
+
+
+<a href="{{ $href }}" class="{{ $active ? 'bg-red-200' : '' }} 
+	mr-4 text-blue-700 hover:underline" {{ $attributes }}>
+    {{ $slot }}
+</a>
